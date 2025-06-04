4bb5894e069d2295c8caab28e7128d382f949083
RomainC76 <105057597+RomainC76@users.noreply.github.com>
2025-06-04 21:32:00 +0200

    Add files via upload

---
+++ script.js allo.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
